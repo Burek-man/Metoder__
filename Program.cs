f687b93e14533cfe4d2d062899db6f0f5f9719e0
Burek-man <alnu07001@utb.vaxjo.se>
2024-09-25 11:18:40 +0200

    metoder

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+
+double radie=2.4;
+Console.WriteLine("crikelns omkrets:"+CirkelOmkrets(radie));
+Console.WriteLine("crikelns area:"+CirkelArea(radie));
+
+double CirkelOmkrets(double radie)
+{
+    return radie*2*Math.PI;
+}
+
+double CirkelArea(double radie)
+{
+    return Math.PI*radie*radie;
+}
+
+*/
+Console.WriteLine();
+double Beräknamedelväre(double tal1, double tal2, double tal3)
+{
+
+return (tal1+tal2+tal3) /3;
+
+}
